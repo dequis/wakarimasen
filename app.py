88b898a02a9b6b13845d9076290d4787d6fbac01
dequis <dx@dxzone.com.ar>
2010-05-08 22:28:02 -0300

    app: task_post

--- app.py
+++ app.py
@@ -12,6 +12,26 @@ def init_database():
     model.metadata.create_all()
     # TODO: cleanup backups table
 
+def task_post(environ, start_response):
+    request = environ['werkzeug.request']
+    board = environ['waka.board']
+
+    params = ['parent', 'field1', 'email', 'subject', 'comment',
+        'password', 'nofile', 'captcha', 'wakaadmin', 'no_captcha',
+        'no_format', 'sticky', 'lock', 'adminpost']
+    kwargs = {}
+    for param in params:
+        kwargs[param] = request.form.get(param, '')
+
+    kwargs['file'] = request.files['file']
+    kwargs['name'] = kwargs.pop('field1')
+    kwargs['admin'] = kwargs.pop('wakaadmin')
+    kwargs['admin_post_mode'] = kwargs.pop('adminpost')
+    kwargs['oekaki_post'] = kwargs['srcinfo'] = kwargs['pch'] = None
+    kwargs['environ'] = environ
+    
+    return board.post_stuff(**kwargs)
+
 def error(environ, start_response, error):
     start_response('200 OK', [('Content-Type', 'text/html')])
     return Template('error_template', error=error.message, environ=environ)
