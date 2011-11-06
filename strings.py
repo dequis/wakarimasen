0250c7fdb351b2b82d7801d31a1ed66829340fc7
Nathan Wilson <who.knows.what.life.is@gmail.com>
2011-11-05 20:56:26 -0400

    String added for indicating inusufficanch privalodjianos.

--- strings.py
+++ strings.py
@@ -188,6 +188,7 @@ NEWFILE = "New File"                                         # Prompt for replac
 STRINGFIELDMISSING = "Please input string to ban."
 MODDELETEONLY = "This was posted by a moderator or admin and cannot be deleted this way."
 POSTNOTFOUND = "Post %d not found on %s."
+INUSUFFICENTPRIVLEDGES = 'Insufficient privileges'
 
 #
 # Oekaki
@@ -216,6 +217,3 @@ OEKPAINTERS = [
         {"painter": "shi_norm_selfy", "name": "Shi Normal+Selfy"},
         {"painter": "shi_pro_selfy", "name": "Shi Pro+Selfy"},
 ]
-
-
-
