45e8c2672c8d4eaab94bd5d0e60a9cb80b565e77
dequis <dx@dxzone.com.ar>
2017-01-04 14:56:42 -0300

    wakapost: turn 'image' into a property that aliases to 'filename'
    Fixes issues with post editor removing the image

--- wakapost.py
+++ wakapost.py
@@ -13,7 +13,7 @@ class WakaPost(object):
     __slots__ = [
         # columns copied directly from model.board
         'num', 'parent', 'timestamp', 'lasthit', 'ip', 'date', 'name', 'trip',
-        'email', 'subject', 'password', 'comment', 'image', 'size', 'md5',
+        'email', 'subject', 'password', 'comment', 'size', 'md5',
         'width', 'height', 'thumbnail', 'tn_width', 'tn_height', 'lastedit',
         'lastedit_ip', '_admin_post', 'stickied', 'locked',
         # extensions
@@ -170,6 +170,14 @@ class WakaPost(object):
     def admin_post(self, value):
         # TODO: database migration / unfucking
         self._admin_post = (value in (True, 1, 'yes', 'True', '1'))
+
+    @property
+    def image(self):
+        return self.filename
+
+    @image.setter
+    def image(self, value):
+        self.filename = value
 
     def set_ip(self, numip, editing=None):
         '''Sets the ip or the lastedit ip'''
